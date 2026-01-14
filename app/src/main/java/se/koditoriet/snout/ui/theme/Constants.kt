158502471bdd791373fd5290a2d45af984a66168
Tham <ph.th4m@gmail.com>
2026-01-14 12:54:33 +0100

    Cleanup

--- app/src/main/java/se/koditoriet/snout/ui/theme/Constants.kt
+++ app/src/main/java/se/koditoriet/snout/ui/theme/Constants.kt
@@ -3,8 +3,6 @@ package se.koditoriet.snout.ui.theme
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-// https://www.learnui.design/blog/android-material-design-font-size-guidelines.html
-
 // Generic
 val BUTTON_FONT_SIZE = 14.sp
 val INPUT_FIELD_PADDING = 12.dp
